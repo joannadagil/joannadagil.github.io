ef47a5db5d20193e5b268da7884fb3f949c5f167
joannadagil <asiadagil@gmail.com>
2025-11-02 20:36:11 +0100

    calc fix

--- calculator.js
+++ calculator.js
@@ -1,39 +1,45 @@
 const display = document.getElementById('display');
 
-    function append(value) {
-    
-      let lastChar = display.value.slice(-1);
+function append(value) {
 
-      if ('+-*/'.includes(value)) {
-        if ('+-*/'.includes(lastChar)) {
-            // Do nothing
-        } else {
-            let correct = true;
-            let finished = false;
-            let i = -1;
-            while (!finished) {
-                lastChar = display.value.slice(i, i+1);
-                if (lastChar === '.') {
-                    finished = true;
-                    correct = false;
-                }
-            }
-            if (correct) {
-                display.value += value;
-            }
-        }
-      } else {
-        display.value += value;
+  let lastChar = display.value.slice(-1);
+
+  if ('+-*/'.includes(value)) {
+    if ('+-*/'.includes(lastChar) || lastChar === '') {
+      // Do nothing
+    } else {
+      display.value += value;
+    }
+  } else if (value === '.') {
+    let correct = true;
+    let finished = false;
+    let i = -1;
+    while (!finished) {
+      lastChar = display.value.slice(i, i+1);
+      if (lastChar === '.') {
+        finished = true;
+        correct = false;
+      }
+      i--;
+      if ('+-*/'.includes(lastChar) || lastChar === '') {
+        finished = true;
       }
     }
-    function C() {
-      display.value = '';
+    if (correct) {
+      display.value += value;
     }
-    function res() {
-      try {
-        let expression = eval(display.value);
-        display.value = expression;
-      } catch (error) {
-        display.value = 'Error';
-      }
-    }
+  } else {
+    display.value += value;
+  }
+}
+function C() {
+  display.value = '';
+}
+function res() {
+  try {
+    let expression = eval(display.value);
+    display.value = expression;
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
